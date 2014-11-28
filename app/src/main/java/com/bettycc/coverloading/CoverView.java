6f1238609caad4df96ccc78f76f8c15f1f92658b
ufo22940268 <ufo22940268@gmail.com>
2014-11-28 15:05:53 +0800

    up

--- app/src/main/java/com/bettycc/coverloading/CoverView.java
+++ app/src/main/java/com/bettycc/coverloading/CoverView.java
@@ -3,12 +3,17 @@ package com.bettycc.coverloading;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Region;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
@@ -26,13 +31,17 @@ public class CoverView extends ImageView {
     private Paint transparentPaint;
     private float mOuterCircleRadius;
     private float mInnerCircleRadius;
+    private float mPauseCircleRadius;
     private int mArcStart;
     private ValueAnimator mRotateAnimator;
+    private float mPauseIconHeight;
+    private float mPauseIconWidth;
 
     public CoverView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
+
     private ValueAnimator.AnimatorUpdateListener mRotateListener = new ValueAnimator.AnimatorUpdateListener() {
         @Override
         public void onAnimationUpdate(ValueAnimator animation) {
@@ -48,6 +57,9 @@ public class CoverView extends ImageView {
     private void init(Context context, AttributeSet attrs) {
         mOuterCircleRadius = getResources().getDimension(R.dimen.outer_circle_radius);
         mInnerCircleRadius = getResources().getDimension(R.dimen.inner_circle_radius);
+        mPauseCircleRadius = mInnerCircleRadius * 0.7f;
+        mPauseIconHeight = getResources().getDimension(R.dimen.pause_icon_height);
+        mPauseIconWidth = getResources().getDimension(R.dimen.pause_icon_width);
 
         mRotateAnimator = ValueAnimator.ofInt(-90, 270);
         mRotateAnimator.setDuration(3000);
@@ -58,6 +70,12 @@ public class CoverView extends ImageView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        Paint redPaint = new Paint();
+        redPaint.setColor(Color.RED);
+
+        Paint greenPaint = new Paint();
+        greenPaint.setColor(Color.GREEN);
 
         bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
         bitmap.eraseColor(Color.TRANSPARENT);
@@ -77,6 +95,7 @@ public class CoverView extends ImageView {
         tempCanvas.drawCircle(cx, cy, mOuterCircleRadius, transparentPaint);
 
         Paint shadowPaint = new Paint();
+        shadowPaint.setAntiAlias(true);
         shadowPaint.setColor(SHADOW_COLOR);
         RectF rectF = new RectF(cx - mInnerCircleRadius,
                 cy - mInnerCircleRadius,
@@ -88,6 +107,45 @@ public class CoverView extends ImageView {
                 true,
                 shadowPaint);
 
+        //Draw pause icon.
+        tempCanvas.drawCircle(cx, cy, mPauseCircleRadius, transparentPaint);
+
+        Bitmap pauseBitmap = Bitmap.createBitmap((int) mPauseCircleRadius * 2, (int) mPauseCircleRadius * 2, Bitmap.Config.ARGB_8888);
+        Canvas pauseCanvas = new Canvas(pauseBitmap);
+
+        int pcx = (int) (mPauseCircleRadius);
+        int pcy = (int) (mPauseCircleRadius);
+
+        Path path = new Path();
+        path.addCircle(pcx, pcy, mPauseCircleRadius, Path.Direction.CCW);
+//        pauseCanvas.clipPath(path, Region.Op.REPLACE);
+        pauseCanvas.drawCircle(pcx, pcy, mPauseCircleRadius, shadowPaint);
+
+        //Draw pause1.
+        RectF pause1 = new RectF();
+        pause1.left = pcx - mPauseIconWidth / 2;
+        pause1.right = pcx + mPauseIconWidth / 2;
+        pause1.top = pcx - mPauseIconHeight / 2;
+        pause1.bottom = pcx + mPauseIconHeight / 2;
+        pauseCanvas.drawRect(pause1, greenPaint);
+
         canvas.drawBitmap(bitmap, 0, 0, null);
+        canvas.drawBitmap(pauseBitmap, cx - mPauseCircleRadius, cx - mPauseCircleRadius, null);
     }
+
+
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//        Paint redPaint = new Paint();
+//        redPaint.setColor(Color.RED);
+//
+//        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+//        Canvas c1 = new Canvas(bitmap);
+//        c1.drawRect(0, 0, 100, 100, redPaint);
+//        Path path = new Path();
+//        path.addCircle(50, 50, 30, Path.Direction.CCW);
+//        canvas.clipPath(path, Region.Op.REPLACE);
+//        canvas.drawBitmap(bitmap, 0, 0, null);
+//    }
 }
